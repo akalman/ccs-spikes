ba014f7c163a2e0d5c7643102a7111fcee6bc4ee
akalman <dkeka.zkfaks@gmail.com>
2016-02-14 14:09:43 -0800

    add in base spike stuff

---
+++ Assets/InputScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputScript : MonoBehaviour {
+    private CharacterController _controller;
+
+    // Use this for initialization
+    void Start () {
+	    _controller = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var vector = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            vector += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            vector += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            vector += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            vector += new Vector3(1, 0, 0);
+        }
+
+        _controller.SimpleMove(vector);
+    }
+}
---
+++ Assets/InputScript2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputScript2 : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vector = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            vector += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            vector += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            vector += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            vector += new Vector3(1, 0, 0);
+        }
+
+        _controller.SimpleMove(vector);
+    }
+}
