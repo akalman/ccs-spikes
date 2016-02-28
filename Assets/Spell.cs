947dbbf6bfdb22ce3f7d25912655f3a0c1fc527f
Matt Mullen <malaguy@gmail.com>
2016-02-28 15:12:42 -0800

    initial working on the spell reticule, with locking

--- Assets/Spell.cs
+++ Assets/Spell.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class Spell : MonoBehaviour {
+public interface ISpell {
 
-	// Use this for initialization
-	void Start () {
+    ISpell fuseWith(ISpell other);
+
+    SpellEffect effect();
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+}
+
+public struct SpellEffect
+{
+    public float damage;
+    public float radius;
+    public Element element;
+
+    public override string ToString()
+    {
+        return "damage: " + damage + ", radius: " + radius + ", element: " + element;
+    }
+}
+
+public enum Element
+{
+    Fire,
+    Ice,
+    Lightning,
+    Adam,
+    White
+}
+
+public class PerfectSpell : ISpell
+{
+    private SpellEffect _effect = new SpellEffect { damage = 100.0f, radius = 10.0f, element = Element.Adam };
+
+    public SpellEffect effect()
+    {
+        return _effect;
+    }
+
+    public ISpell fuseWith(ISpell other)
+    {
+        // fuck other spells
+        return this;
+    }
 }
